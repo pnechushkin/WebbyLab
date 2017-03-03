44a187c2b4fd8d1024d092a3dce2599703754a9d
pnechushkin <pnechushkin@gmail.com>
2017-03-03 13:53:27 +0200

    WebbyLab

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Class Animal
+ */
+class Animal
+{
+    /**
+     * @var string
+     */
+    protected $name='';
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name.'</br>';
+    }
+
+    /**
+     * Animal constructor.
+     * @param $name
+     */
+    function __construct($name) {
+        $this->name=$name;
+    }
+}
+/**
+ * Class Cat
+ */
+class Cat extends Animal
+{
+    /**
+     * @var string
+     */
+    protected $catname='';
+
+    /**
+     * @return string
+     */
+    public function meow ()
+    {
+        return "Cat {$this->catname} is sayig meow</br>";
+    }
+
+    /**
+     * Cat constructor.
+     * @param $catname
+     */
+    function __construct($catname) {
+        parent::__construct($catname);
+        $this->catname=$this->name;
+    }
+}
+
+
+$cat = new Cat('Test Name');
+echo $cat->getName ();
+echo $cat->meow ();
